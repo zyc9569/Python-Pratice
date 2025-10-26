cdaad57423d01bebdbdca50165c0c1380a11f37f
Snorar <2537206985@qq.com>
2025-10-26 20:35:13 +0800

    Add files via upload

---
+++ source code of Czy.py
@@ -0,0 +1,22 @@
+# The source code of Czy
+while True:
+    topic = input("输入你想聊天的话题：")
+    ideal={"原神","崩铁","何佳敏","瓦罗兰特","鸣潮","辣椒素","明日方舟"}
+    if topic == "原神":
+        print("给你玩的高练度胡桃爽号")
+    elif topic == "崩铁":
+        print("看我打0T混沌好吧(附上链接：https://www.bilibili.com/video/BV195uHzRE4K/?spm_id_from=333.1387.homepage.video_card.click)")
+    elif topic == "何佳敏":
+        print("我草你妈，我恨你们")
+        print("没说过就是没说过，没发生就是没发生")
+        print("没发生的事情我为什么要承认")
+    elif topic == "瓦罗兰特":
+        print("带你打瓦，看我颗秒，我已经会瞄头了好吧")
+    elif topic == "鸣潮":
+        print("垃圾游戏")
+    elif topic == "辣椒素":
+        print("辣椒素你个人机")
+    elif topic == "明日方舟":
+        print("saki酱！saki酱！saki酱！")
+    elif topic not in ideal:
+        print("我不想聊这个话题,让我们换个话题吧，比如怪猎")
