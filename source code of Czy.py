6c77d2dbb04325f6c93fd13a468057d35d6b7765
Snorar <2537206985@qq.com>
2025-11-15 18:01:45 +0800

    Update source code of Czy.py

--- source code of Czy.py
+++ source code of Czy.py
@@ -1,7 +1,7 @@
 # The source code of Czy
 while True:
     topic = input("输入你想聊天的话题：")
-    ideal={"原神","崩铁","何佳敏","瓦罗兰特","鸣潮","辣椒素","明日方舟"}
+    ideal={"原神","崩铁","瓦罗兰特","鸣潮","辣椒素","明日方舟","三国杀"}
     if topic == "原神":
         print("给你玩的高练度胡桃爽号")
     elif topic == "崩铁":
@@ -14,6 +14,9 @@ while True:
         print("辣椒素你个人机")
     elif topic == "明日方舟":
         print("saki酱！saki酱！saki酱！")
+    elif topic == "三国杀"
+        print("看我乱杀！！！")
     elif topic not in ideal:
         print("我不想聊这个话题,让我们换个话题吧，比如怪猎")
+
 
